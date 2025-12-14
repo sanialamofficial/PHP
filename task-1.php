90a77edf8fee21f1922b2b811891efc98d590426
Sani <sanialamofficial@gmail.com>
2025-12-14 14:08:06 +0600

    hi

---
+++ task-1.php
@@ -0,0 +1,17 @@
+<?php
+
+$length = 10;
+$width = 5;
+
+
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+
+echo "Length: " . $length . "<br>";
+echo "Width: " . $width . "<br>";
+echo "Area of Rectangle: " . $area . "<br>";
+echo "Perimeter of Rectangle: " . $perimeter;
+
+
+?>
---
+++ task-2.php
@@ -0,0 +1,8 @@
+<?php
+
+$value=300;
+
+$vat=$value *  0.15;
+
+echo " the vated value " . $vat. "<br>";
+?>
